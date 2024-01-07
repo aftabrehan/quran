8c22d7877fd58d040f8342e875a5ce92518c9d05
Mohammad Faizan <faizancurious@gmail.com>
2024-01-08 00:47:58 +0530

    All Surah list with Sorting

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,90 @@
 "use client"
 
+import { useState, useEffect } from "react"
 import { useToast } from "@/components/ui/use-toast"
+import ListSurah from "@/components/listsurah"
+import { FaArrowUp91 } from "react-icons/fa6";
+import { Skeleton } from "@/components/ui/skeleton"
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 
 export default function Home() {
   const { toast } = useToast()
+  const [listSurah, setListSurah] = useState([])
+  const [sort, setSort] = useState("acc")
+
+  useEffect(() => {
+    fetchData()
+  }, [sort])
+
+  const fetchData = async () => {
+    try {
+      fetch("/api/listsurah")
+        .then((res) => res.json())
+        .then((data) => {
+          sortData(data.data)
+        })
+    } catch (err: any) {
+      toast({
+        title: "Error",
+        description: err.message,
+      })
+    }
+  }
+
+  const sortData = (data: any) => {
+    if (sort === "acc") {
+      setListSurah(data.sort((a: any, b: any) => a.number - b.number))
+    } else {
+      setListSurah(data.sort((a: any, b: any) => b.number - a.number))
+    }
+  }
 
   return (
-    <main className="w-full min-h-screen flex items-center justify-center">
-      <div className="w-2/4 items-center min-h-screen">
-        Main Body
+    <main className="w-full flex items-center justify-center">
+      <div className="w-full lg:w-2/3 mx-4 md:mx-7 lg:mx-10 items-center mt-5 mb-5">
+        <div className="flex justify-between items-center">
+          <h1 className="text-4xl font-semibold">List Surah <span className="text-xs text-gray-400">Total: {listSurah.length}</span></h1>
+          {/* Sort By dropdown */}
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <p className="flex items-center gap-1 cursor-pointer px-2 py-[5px] border rounded-md">Sort By <FaArrowUp91 /></p>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-56">
+              <DropdownMenuLabel>Sort By</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuRadioGroup value={sort} onValueChange={setSort}>
+                <DropdownMenuRadioItem value="acc">Accending</DropdownMenuRadioItem>
+                <DropdownMenuRadioItem value="dec">Decending</DropdownMenuRadioItem>
+              </DropdownMenuRadioGroup>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+        {/* // if large screen 3 grid else 1 grid */}
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 mt-5">
+          {listSurah.length > 0 ? (
+            listSurah.map((item) => (
+              <ListSurah key={item} data={item} />
+            ))
+          ) : (
+            <>
+            <Skeleton className="w-full h-20 rounded-md" />
+            <Skeleton className="w-full h-20 rounded-md" />
+            <Skeleton className="w-full h-20 rounded-md" />
+            <Skeleton className="w-full h-20 rounded-md" />
+            <Skeleton className="w-full h-20 rounded-md" />
+            <Skeleton className="w-full h-20 rounded-md" />
+            </>
+          )}
+        </div>
       </div>
     </main>
   )
---
+++ components/listsurah.tsx
@@ -0,0 +1,29 @@
+"use client"
+
+import React from 'react'
+import Link from 'next/link'
+
+interface Props {
+    data: any
+}
+
+const ListSurah = ({ data }: Props) => {
+    console.log(data)
+    return (
+        <Link href={''} className='border p-4 hover:border-[#22A5AD] flex justify-between items-center rounded-md'>
+            <div className='flex gap-2 items-center justify-center'>
+                <span className='flex mr-2 bg-gray-300 dark:bg-gray-800/30 hover:bg-[#22A5AD] w-12 h-12 rounded-lg text-center items-center justify-center rotate-45'><p className='-rotate-45 text-black dark:text-white'>{data.number}</p></span>
+                <div className='flex flex-col'>
+                    <p className='font-semibold text-lg'>{data.englishName}</p>
+                    <p className='text-sm text-gray-400'>{data.englishNameTranslation}</p>
+                </div>
+            </div>
+            <div className='flex flex-col text-end'>
+                <p>{data.name}</p>
+                <p className='text-sm text-gray-400'>{data.numberOfAyahs} Aayahs</p>
+            </div>
+        </Link>
+    )
+}
+
+export default ListSurah
