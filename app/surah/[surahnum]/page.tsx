665f06f38b7ad6e46a75ba1943a770e6667e9fb5
Mohammad Faizan <faizancurious@gmail.com>
2024-01-08 10:50:31 +0530

    Bismillah Header

--- app/surah/[surahnum]/page.tsx
+++ app/surah/[surahnum]/page.tsx
@@ -1,17 +1,19 @@
 import React from "react";
 
 export default async function page({
-  params,
+    params,
 }: {
-  params: { surahnum: string };
+    params: { surahnum: string };
 }) {
-  // const session = await getServerAuthSession();
-  const surahnum = await params.surahnum;
-  
+    // const session = await getServerAuthSession();
+    const surahnum = await params.surahnum;
 
-  return (
-    <div>
-        <h1>Surah {surahnum}</h1>
-    </div>
-  );
+
+    return (
+        <main className="flex w-full max-h-screen items-center justify-center">
+            <div className="w-full lg:w-2/3 mx-4 md:mx-7 lg:mx-10 mt-5 mb-5 h-full items-center justify-center">
+                <p className="font-uthmanic h-auto text-center font-bold text-4xl">بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ</p>
+            </div>
+        </main>
+    );
 }
